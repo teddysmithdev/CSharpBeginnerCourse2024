10ea4a1af35615c57993aba0402f3783010a910c
teddysmithdev <teddysmithdev@gmail.com>
2024-02-28 10:23:46 -0500

    Strings In-Depth

--- Program.cs
+++ Program.cs
@@ -1,5 +1,37 @@
-﻿string name = "teddy";
+﻿//CRUD
+//Create
+using System.Text;
 
-long jeffBezosNetworth = 196000000000;
+var petFish = "puffer fish";
 
-var charchar = 'c';
+//Create - Conatentation
+petFish = "porcupine " + petFish;
+
+//Create - Template Literal
+Console.WriteLine($"In my salt water aquarium, I will have a {petFish}");
+
+//Create - @ Literal
+var pathName = @"C:\Users\teddy\OneDrive";
+
+//READ
+Console.WriteLine(petFish);
+
+//Update
+//petFish[0] = "p" (this is not possible)
+Console.WriteLine(petFish.Replace("porcupine", "blue dot"));
+
+
+//Delete (Not technically a delete but when you are worried about memory do this)
+StringBuilder newCrustacean = new StringBuilder();
+newCrustacean.Append("red lobster");
+Console.WriteLine(newCrustacean);
+newCrustacean.Replace("red", "blue");
+Console.WriteLine(newCrustacean);
+
+
+
+
+
+
+
+
