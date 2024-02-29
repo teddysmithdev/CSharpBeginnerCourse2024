148b725d5c25f4c6b02e251185d1d1ee8d8bc358
teddysmithdev <teddysmithdev@gmail.com>
2024-02-29 10:27:34 -0500

    Arrays

--- Program.cs
+++ Program.cs
@@ -1,37 +1,25 @@
-﻿//CRUD
+﻿//Array CRUD
+
 //Create
-using System.Text;
+string[] favoriteRats = ["fancy rat", "brown rat", "radioactive rat", "wolf rat"];
 
-var petFish = "puffer fish";
-
-//Create - Conatentation
-petFish = "porcupine " + petFish;
-
-//Create - Template Literal
-Console.WriteLine($"In my salt water aquarium, I will have a {petFish}");
-
-//Create - @ Literal
-var pathName = @"C:\Users\teddy\OneDrive";
-
-//READ
-Console.WriteLine(petFish);
+//Read
+//Console.WriteLine(favoriteRats);
+// foreach(var rat in favoriteRats) 
+// {
+//     Console.WriteLine(rat);
+// }
 
 //Update
-//petFish[0] = "p" (this is not possible)
-Console.WriteLine(petFish.Replace("porcupine", "blue dot"));
+//favoriteRats[0] = "Fancy Rat";
+// LINQ - "update array linq"
 
+var newFavoriteRats = favoriteRats.Where((e) => e.StartsWith("b"));
 
-//Delete (Not technically a delete but when you are worried about memory do this)
-StringBuilder newCrustacean = new StringBuilder();
-newCrustacean.Append("red lobster");
-Console.WriteLine(newCrustacean);
-newCrustacean.Replace("red", "blue");
-Console.WriteLine(newCrustacean);
+foreach(var rat in newFavoriteRats) 
+{
+    Console.WriteLine(rat);
+}
 
-
-
-
-
-
-
-
+//Delete
+//gimme
