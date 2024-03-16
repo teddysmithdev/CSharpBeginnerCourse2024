8a592ed1fd694a8f46e1138ea89c65f1d2d67404
teddysmithdev <teddysmithdev@gmail.com>
2024-03-16 11:59:13 -0400

    Methods

--- Program.cs
+++ Program.cs
@@ -1,32 +1,10 @@
 ﻿using ConsoleApp;
 
-//CRUD
+RealEstate elmStreet = new()
+{
+    Address = "Elm Street",
+    SquareFootage = 1300,
+    Price = 300000
+};
 
-Rat wolfRat = new Rat();
-Rat fancyRat = new Rat();
-
-wolfRat.Name = "Wolf Rat";
-wolfRat.Number = 1000;
-wolfRat.IsRadioactive = false;
-
-fancyRat.Name = "Fancy Rat";
-fancyRat.Number = 500;
-fancyRat.IsRadioactive = false;
-
-//Read
-Console.WriteLine(wolfRat.Name);
-Console.WriteLine(fancyRat.Name);
-
-//Update
-wolfRat.Name = "WOLF RAT";
-fancyRat.Name = "FANCY RAT";
-
-Console.WriteLine(wolfRat.Name);
-Console.WriteLine(fancyRat.Name);
-
-//Delete
-wolfRat.Name = "";
-fancyRat.Name = "";
-
-Console.WriteLine(wolfRat.Name);
-Console.WriteLine(fancyRat.Name);
+Console.WriteLine(elmStreet.CalcuatePricePerSquareFoot());
