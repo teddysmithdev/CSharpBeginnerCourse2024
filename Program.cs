1bfa608b042341329559049d1a6c659b27be28ee
teddysmithdev <teddysmithdev@gmail.com>
2024-03-29 12:44:19 -0400

    List

--- Program.cs
+++ Program.cs
@@ -1,6 +1,26 @@
-﻿using ConsoleApp;
+﻿//CRUD
+//Create
+List<int> numNums = [1, 2, 3, 4, 5];
 
-TimeUtility.PrintTime();
-TimeUtility.PrintTime();
-TimeUtility.PrintTime();
-TimeUtility.PrintTime();
+//Read
+// foreach (var num in numNums)
+// {
+//     Console.WriteLine(num);
+// }
+
+//Update
+//This is the fastest
+//numNums.Add(0);
+//insert
+//numNums.Insert(0, 0);
+
+//Delete
+//var newNum = numNums.Where((i) => i != 2);
+
+//numNums.Remove(1);
+numNums.RemoveAt(0);
+
+foreach (var num in numNums)
+{
+    Console.WriteLine(num);
+}
