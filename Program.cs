ba305d949f6287cbb76d8c99ced0820393bb0848
teddysmithdev <teddysmithdev@gmail.com>
2024-04-19 11:05:35 -0400

    Records

--- Program.cs
+++ Program.cs
@@ -1,26 +1,18 @@
-﻿//CRUD
-//Create
-List<int> numNums = [1, 2, 3, 4, 5];
+﻿Person person1 = new("Teddy", "Smith");
+Person person2 = new("Teddy", "Smith");
 
-//Read
-// foreach (var num in numNums)
-// {
-//     Console.WriteLine(num);
-// }
+//Shallow Immutabiility
+//person1.LastName = "smith";
+//person1.PhoneNumbers[0] = "222-2222";
 
-//Update
-//This is the fastest
-//numNums.Add(0);
-//insert
-//numNums.Insert(0, 0);
+//Equality
+//Value - 1. Check the type 2. Check the contents
+//Referential - 2. By memory (hex code)
+Console.WriteLine(person1 == person2);
 
-//Delete
-//var newNum = numNums.Where((i) => i != 2);
+//Non-destructive Mutation (Copy)
+Person person3 = person2 with { LastName = "Brewski" };
+Console.WriteLine(person3.ToString());
 
-//numNums.Remove(1);
-numNums.RemoveAt(0);
 
-foreach (var num in numNums)
-{
-    Console.WriteLine(num);
-}
+public record Person(string FirstName, string LastName);
